735200f14a9a4d2ef3156ef8f8520c5ca331700f
Oreo <lala21525@gmail.com>
2015-04-01 16:33:28 +0800

    Initial Commit

---
+++ categoryTest1/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  categoryTest1
+//
+//  Created by 李育豪 on 2015/4/1.
+//  Copyright (c) 2015年 ALPHACamp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
